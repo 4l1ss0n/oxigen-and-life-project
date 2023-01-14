d7643f44ef9cbd12014e03d4b2ea8a29c412dcd9
Alisson-Py <alissilva1231@hotmail.com>
2023-01-13 21:15:35 -0300

    Feat: create Materials page

---
+++ src/pages/Materiais/index.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import "./Styles.css";
+import Foward from "../../assets/undo.svg";
+import { items } from "../../assets/contentExemples";
+
+
+export default function Materials() {
+  const data: any[]= items;
+  return (
+    <div className="materials">
+      <div className="content-header">
+        <p className="path">
+          <img src={Foward} alt="foward" />
+          Estoque/Unidates de Alocação</p>
+        <div className="content-header-title-and-buttons">
+          <h1 className="title">Materiais</h1>
+          <div className="button-content">
+            <button className="button" >Exportar em CSV</button>
+            <button className="button" >Novo Material</button>
+          </div>
+        </div>
+      </div>
+      <div className="filter-content">
+        <p className="divition-bar-text">Filtro</p>
+        <div className="filter-selection">
+          <div className="input-view">
+            <label htmlFor="item-name-input">Nome</label>
+            <input type="text" name="item-name-input" id="item-name-input"/>
+          </div>
+          <p className="filter-selection-text-middle">OU</p>
+          <div className="input-view">
+            <label htmlFor="item-name-input">Cod</label>
+            <input type="text" name="item-code-input" id="item-code-input"/>
+          </div>
+        </div>
+      </div>
+
+      <div className="unit-list-content">
+          <table className="unit-table">
+            <tr className="unit-table-title">
+              <th className="unit-table-title-text">Nome</th>
+              <th className="unit-table-title-text">Tamanho</th>
+              <th className="unit-table-title-text">Unidade</th>
+              <th className="unit-table-title-text">Cor</th>
+              <th className="unit-table-title-text">Preço de custo</th>
+              <th className="unit-table-title-text">Ação</th>
+            </tr>
+            {
+              data.map(list => (
+                <tr className="unit-table-list" key={list.id}>
+                  <td className="unit-table-list-text">{list.name}</td>
+                  <td className="unit-table-list-text">{list.size}</td>
+                  <td className="unit-table-list-text">{list.unit}</td>
+                  <td className="unit-table-list-text">{list.color}</td>
+                  <td className="unit-table-list-text">{list.price}</td>
+                  <td className="unit-table-list-text">Editar | Excluir</td>
+                </tr>
+              ))
+            }
+          </table>
+      </div>
+    </div>
+  );
+};
