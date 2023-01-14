f2197ea2578731f00ac630eaeffa2f20b8ec76d4
Alisson-Py <alissilva1231@hotmail.com>
2023-01-13 21:13:40 -0300

    Feat: create Add Clients Page

---
+++ src/pages/AddClients/index.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+import "./Styles.css";
+import Foward from "../../assets/undo.svg";
+
+export default function AddClients() {
+  return (
+    <div className="add-clients">
+      <div className="content-header">
+        <p className="path">
+          <img src={Foward} alt="foward" />
+          Clientes/adicionar clientes</p>
+        <div className="content-header-title">
+          <h1 className="title">Adicionar Clientes</h1>
+        </div>
+      </div>
+      <div className="add-clients-form">
+        <form className="form-view">
+          <div className="input-view">
+            <p className="input-tittle">Numero de identificação</p>
+            <input type="text" className="input" id="identification-number-input"/>
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Classe</p>
+            <input type="text" className="input" id="class-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Convênio</p>
+            <input type="text" className="input" id="convenio-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Tipo</p>
+            <input type="text" className="input" id="convenio-type-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Status</p>
+            <input type="text" className="input" id="status-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">CPF/CNPJ</p>
+            <input type="text" className="input" id="cpf-cnpj-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Nome</p>
+            <input type="text" className="input" id="name-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Telefone</p>
+            <input type="text" className="input" id="phone-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">CEP</p>
+            <input type="text" className="input" id="cep-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Logradouro</p>
+            <input type="text" className="input" id="postal-code-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Nº</p>
+            <input type="text" className="input" id="house-number-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Bairro</p>
+            <input type="text" className="input" id="neibhor-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Cidade</p>
+            <input type="text" className="input" id="city-input" />
+          </div>
+          <div className="input-view">
+            <p className="input-tittle">Estado</p>
+            <input type="text" className="input" id="state-input" />
+          </div>
+          <div className="button-view">
+            <button type="submit" className="form-button">Cadastrar Cliente</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
