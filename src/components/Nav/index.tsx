b396759d2c40c03bd47378daaff038900a8c7a3b
Alisson-Py <alissilva1231@hotmail.com>
2023-01-14 21:02:26 -0300

    Feat: new items arrange

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./Styles.css";
 
 import Home from "../../assets/home.svg";
 import Store from "../../assets/store.svg";
+import User from "../../assets/user.svg";
 
 const Nav = () => {
   return (
@@ -17,20 +19,59 @@ const Nav = () => {
       <div className="link-navigation-view">
         <div className="link-navigation-header">
           <img src={Store} alt="Store" className="link-img" />
+          <p className="link-text">Unidades de Alocação</p>
+        </div>
+        <div className="link-navigation-sub-class">
+          <NavLink to="/alocation-unit" className="link-text">Unidades de Alocação</NavLink>
+          <NavLink to="/allocation-unit-add" className="link-text">Cadastrar UA</NavLink>
+        </div>
+      </div>
+      <div className="link-navigation-view">
+        <div className="link-navigation-header">
+          <img src={Store} alt="Store" className="link-img" />
           <p className="link-text">Estoque</p>
         </div>
         <div className="link-navigation-sub-class">
-          <p className="link-text">Cadastrar UA</p>
-          <p className="link-text">Movimentação de items</p>
-          <p className="link-text">Novas Aquisições</p>
-          <p className="link-text">Itens de Estoque</p>
-          <p className="link-text">Dar Baixa de Itens</p>
-          <p className="link-text">Recargas de Cilíndros</p>
-          <p className="link-text">Relatórios de Estoque</p>
+          <NavLink to="/materials" className="link-text">Items</NavLink>
+          <NavLink to="/acquisition" className="link-text">Novas Aquisições</NavLink>
+          <NavLink to="/stock-items" className="link-text">Itens de Estoque</NavLink>
+          <NavLink to="/check-off-items" className="link-text">Dar Baixa de Itens</NavLink>
+          <NavLink to="/stock-reports" className="link-text">Relatórios de Estoque</NavLink>
         </div>
       </div>
-      
-
+      <div className="link-navigation-view">
+        <div className="link-navigation-header">
+          <img src={User} alt="Store" className="link-img" />
+          <p className="link-text">Clientes</p>
+        </div>
+        <div className="link-navigation-sub-class">
+          <NavLink to="/clients" className="link-text">Clientes</NavLink>
+          <NavLink to="/clients-add" className="link-text">Cadastrar Clientes</NavLink>
+          <NavLink to="/agreements" className="link-text">Convênio</NavLink>
+          <NavLink to="/contracts" className="link-text">Contratos</NavLink>
+        </div>
+      </div>
+      <div className="link-navigation-view">
+        <div className="link-navigation-header">
+          <img src={User} alt="Store" className="link-img" />
+          <p className="link-text">Materiais</p>
+        </div>
+        <div className="link-navigation-sub-class">
+          <NavLink to="/materials" className="link-text">Materiais</NavLink>
+          <NavLink to="/provider" className="link-text">Fornecedores</NavLink>
+          <NavLink to="/provider-add" className="link-text">Cadastrar Fornecedor</NavLink>
+        </div>
+      </div>
+      <div className="link-navigation-view">
+        <div className="link-navigation-header">
+          <img src={User} alt="Store" className="link-img" />
+          <p className="link-text">Movimentações</p>
+        </div>
+        <div className="link-navigation-sub-class">
+          <NavLink to="/items-movimentation" className="link-text">Movimentação de items</NavLink>
+          <NavLink to="/cylinder-refill" className="link-text">Recargas de Cilíndros</NavLink>
+        </div>
+      </div>
     </nav>
   );
 };
