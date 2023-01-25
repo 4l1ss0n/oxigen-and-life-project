d794d7f8a141ebf9f573bd04cb3d7f36a8278dc7
Alisson-Py <alissilva1231@hotmail.com>
2023-01-24 22:51:03 -0300

    Feat: Imcrements images

---
+++ src/assets/contentExemples.ts
@@ -0,0 +1,348 @@
+export const Filials = [
+  {
+    id: 1,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 2,
+    name: "UA-SAJ-Laje",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 100
+  },
+  {
+    id: 3,
+    name: "UA-SAJ-Valença",
+    filial: "SAJ",
+    type: "Veiculos",
+    items: 20
+  },
+  {
+    id: 4,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "Clientes",
+    items: 500
+  },
+  {
+    id: 5,
+    name: "UA-SAJ-Laje",
+    filial: "SAJ",
+    type: "Veiculos",
+    items: 5
+  },
+  {
+    id: 6,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 7,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 8,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 9,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 10,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 11,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 12,
+    name: "UA-SAJ-Laje",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 100
+  },
+  {
+    id: 13,
+    name: "UA-SAJ-Valença",
+    filial: "SAJ",
+    type: "Veiculos",
+    items: 20
+  },
+  {
+    id: 14,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "Clientes",
+    items: 500
+  },
+  {
+    id: 15,
+    name: "UA-SAJ-Laje",
+    filial: "SAJ",
+    type: "Veiculos",
+    items: 5
+  },
+  {
+    id: 16,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 17,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 18,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 19,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+  {
+    id: 20,
+    name: "UA-SAJ-centro",
+    filial: "SAJ",
+    type: "centro de distribuição",
+    items: 500
+  },
+];
+
+export const items = [
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  },
+  {
+    id: 1,
+    name: "Oxigenio Gasoso Medicinal 1m³",
+    size: 1,
+    unit: "m³",
+    color: "Verde",
+    price: 100.00,
+  }
+];
+
+export const clients = [
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+  {
+    id: 1,
+    name: "Hospital Nova Saúde",
+    document: "01.669.184/0001-92",
+    city: "SAJ",
+    class: "Convênio",
+    identificationNumber: 64605189
+  },
+];
