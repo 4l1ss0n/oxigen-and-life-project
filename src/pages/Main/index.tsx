99e94dec51890b57e12ccdab769bb0765594537b
Alisson-Py <alissilva1231@hotmail.com>
2023-01-29 13:16:35 -0300

    Feat: Add more routes

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,11 +3,14 @@ import { Routes, HashRouter, Route } from "react-router-dom";
 
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
+import Acquisition from "../Acquisition";
+import AddAllocationUnit from "../AddAllocationUnit";
 import AddClients from "../AddClients";
 
 import AllocationUnits from "../AllocationUnits";
+import CheckOffItems from "../CheckOffItems";
 import Clients from "../Clients";
-import Materials from "../Materiais";
+import Materials from "../Items";
 
 import "./Styles.css";
 
@@ -21,7 +24,14 @@ export default function Main() {
             <Routes>
               <Route path="/" caseSensitive element={<Clients/>} />
               <Route path="/alocation-unit" caseSensitive element={<AllocationUnits/>} />
-              <Route path="/materials" caseSensitive element={<Materials/>} />
+              <Route path="/allocation-unit-add" caseSensitive element={<AddAllocationUnit/>} />
+              
+              <Route path="/stock-items" caseSensitive element={<Materials/>} />
+              <Route path="/acquisition" caseSensitive element={<Acquisition/>} />
+              <Route path="/check-off-items" caseSensitive element={<CheckOffItems/>} />
+
+
+              <Route path="/clients" caseSensitive element={<Clients/>} />
               <Route path="/clients-add" caseSensitive element={<AddClients/>} />
             </Routes>
         </main>
